8b4b9af265b770614351b0bd3a6c6cbe14768dcf
OskarQwert <OskarQwert@noreply.github.com>
2021-01-14 18:50:42 +0000

    Bug Message

---
+++ src/main/java/net/mcreator/peemod/procedures/LlProcedure.java
@@ -0,0 +1,61 @@
+package net.mcreator.peemod.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.peemod.PeemodModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@PeemodModElements.ModElement.Tag
+public class LlProcedure extends PeemodModElements.ModElement {
+	public LlProcedure(PeemodModElements instance) {
+		super(instance, 1);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure Ll!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		String playername = "";
+		if (entity instanceof PlayerEntity && !entity.world.isRemote) {
+			((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("Oskar's pee mod is a VERYYY GLITCHY MOD "), (false));
+		}
+		if (entity instanceof PlayerEntity && !entity.world.isRemote) {
+			((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("Seriously. This can destroy bedrock for now."), (false));
+		}
+		playername = (String) (entity.getDisplayName().getString());
+		if (entity instanceof PlayerEntity && !entity.world.isRemote) {
+			((PlayerEntity) entity).sendStatusMessage(
+					new StringTextComponent((("Anyway, thank you, ") + "" + ((playername)) + "" + (", for playing my mod!"))), (false));
+		}
+	}
+
+	@SubscribeEvent
+	public void onEntityJoin(EntityJoinWorldEvent event) {
+		World world = event.getWorld();
+		Entity entity = event.getEntity();
+		double i = entity.getPosX();
+		double j = entity.getPosY();
+		double k = entity.getPosZ();
+		Map<String, Object> dependencies = new HashMap<>();
+		dependencies.put("x", i);
+		dependencies.put("y", j);
+		dependencies.put("z", k);
+		dependencies.put("world", world);
+		dependencies.put("entity", entity);
+		dependencies.put("event", event);
+		this.executeProcedure(dependencies);
+	}
+}
